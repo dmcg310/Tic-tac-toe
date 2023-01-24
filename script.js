e5305befec27b42a0ed604bbd7484e61d7440861
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-01-24 16:54:41 +0000

    Added function to check for winning combinations

--- script.js
+++ script.js
@@ -18,20 +18,77 @@ const gameboard = () => {
     let turnDisplay = document.querySelector(".turn-display");
     let temp = document.querySelectorAll(".board-display > div");
     turnDisplay.innerText = "X's Turn";
-    for (i = 0; i < temp.length; i++)
+    for (i = 0; i < temp.length; i++) {
       temp[i].addEventListener("click", (e) => {
         if (e.target.innerText == "") {
           if (turn % 2 === 0) {
             e.target.innerText = "O";
             turnDisplay.innerText = "X's Turn";
             turn++;
+            if (e.target.id === "zeroZero") {
+              _board[0][0] = "O";
+            }
+            if (e.target.id === "zeroOne") {
+              _board[0][1] = "O";
+            }
+            if (e.target.id === "zeroTwo") {
+              _board[0][2] = "O";
+            }
+            if (e.target.id === "oneZero") {
+              _board[1][0] = "O";
+            }
+            if (e.target.id === "oneOne") {
+              _board[1][1] = "O";
+            }
+            if (e.target.id === "oneTwo") {
+              _board[1][2] = "O";
+            }
+            if (e.target.id === "twoZero") {
+              _board[2][0] = "O";
+            }
+            if (e.target.id === "twoOne") {
+              _board[2][1] = "O";
+            }
+            if (e.target.id === "twoTwo") {
+              _board[2][2] = "O";
+            }
+            checkScore(_board);
           } else {
             e.target.innerText = "X";
             turnDisplay.innerText = "O's Turn";
             turn++;
+            if (e.target.id === "zeroZero") {
+              _board[0][0] = "X";
+            }
+            if (e.target.id === "zeroOne") {
+              _board[0][1] = "X";
+            }
+            if (e.target.id === "zeroTwo") {
+              _board[0][2] = "X";
+            }
+            if (e.target.id === "oneZero") {
+              _board[1][0] = "X";
+            }
+            if (e.target.id === "oneOne") {
+              _board[1][1] = "X";
+            }
+            if (e.target.id === "oneTwo") {
+              _board[1][2] = "X";
+            }
+            if (e.target.id === "twoZero") {
+              _board[2][0] = "X";
+            }
+            if (e.target.id === "twoOne") {
+              _board[2][1] = "X";
+            }
+            if (e.target.id === "twoTwo") {
+              _board[2][2] = "X";
+            }
           }
         }
       });
+    }
+    checkScore(_board);
   }
 
   return {
@@ -56,5 +113,47 @@ function temporary2() {
   console.log(playerOne.name, playerOne.symbol);
 }
 
+function checkScore(arr) {
+  for (let i = 0; i < 3; i++) {
+    if (
+      arr[i][0] === arr[i][1] &&
+      arr[i][0] === arr[i][2] &&
+      arr[i][0] !== null
+    ) {
+      if (arr[i][0] === "X") {
+        alert("X wins");
+      } else {
+        alert("O wins");
+      }
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (
+      arr[0][i] === arr[1][i] &&
+      arr[0][i] === arr[2][i] &&
+      arr[0][i] !== null
+    ) {
+      if (arr[0][i] === "O") {
+        alert("X wins");
+      } else {
+        alert("O wins");
+      }
+    }
+  }
+
+  if (
+    (arr[0][0] === arr[1][1] &&
+      arr[0][0] === arr[2][2] &&
+      arr[0][0] !== null) ||
+    (arr[0][2] === arr[1][1] && arr[0][2] === arr[2][0] && arr[0][2] !== null)
+  ) {
+    if (arr[0][0] === "X") {
+      alert("X wins");
+    } else {
+      alert("O wins");
+    }
+  }
+}
 temporary();
 temporary2();
